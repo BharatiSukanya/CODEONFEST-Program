78c19ee83d14da17117c4b0ca7735ab4fcdf0bcb
Sukanya Bharati <64637631+BharatiSukanya@users.noreply.github.com>
2021-09-02 17:40:47 +0530

    Update 21. Merge Two Sorted Lists.cpp

--- 21. Merge Two Sorted Lists.cpp
+++ 21. Merge Two Sorted Lists.cpp
@@ -37,3 +37,23 @@ public:
         return res->next;
     }
 };
+
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1==NULL) //in case only one linkedlist exists 
+            return l2;
+        if(l2==NULL)  //in case only one linkedlist exists 
+            return l1;
+        
+        if(l1->val >= l2->val)  // if value in l1 is greater 
+            l2->next = mergeTwoLists(l1,l2->next); //pass l1 value but for l2 iterate to the next value
+        else
+        {
+            l1->next = mergeTwoLists(l1->next,l2);
+            l2=l1;
+        }
+        return l2;
+    }
+};
