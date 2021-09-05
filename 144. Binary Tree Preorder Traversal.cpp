02002de8dfbd635c4447e537d23b519f94e3bc88
Sukanya Bharati <64637631+BharatiSukanya@users.noreply.github.com>
2021-09-05 15:10:56 +0530

    Update 144. Binary Tree Preorder Traversal.cpp

--- 144. Binary Tree Preorder Traversal.cpp
+++ 144. Binary Tree Preorder Traversal.cpp
@@ -38,3 +38,21 @@ public:
         return ans;
     }
 };
+
+For GFG :
+
+void traverse(Node* root, vector<int>& ans) {
+        if(root == NULL) return;
+
+        ans.push_back(root->data);
+        traverse(root->left, ans);
+        traverse(root->right, ans);
+}
+
+vector <int> preorder(Node* root)
+{
+// Your code here
+    vector<int> ans;
+    traverse(root, ans);
+    return ans;
+}
