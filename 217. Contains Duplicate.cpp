9ad885a5d9955927a28b666599e1ca3381a6d1fc
Sukanya Bharati <64637631+BharatiSukanya@users.noreply.github.com>
2021-09-10 20:56:09 +0530

    Update 217. Contains Duplicate.cpp

--- 217. Contains Duplicate.cpp
+++ 217. Contains Duplicate.cpp
@@ -14,9 +14,34 @@ Output: true
   
 Solution :
 
+Time Complexity - O(1) because we are just comparing the size of the array and the hashmap(hashmap does not store duplicate values) 
+Space Complexity - O(n) , it is being used to stoe the array in the hashmap which requires space. 
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         return (nums.size() > unordered_set<int>(nums.begin(), nums.end()).size());     
+    }
+};
+
+Time Comlexity - O(n) where n is the number of elements in the array.
+Space Complexity - O(n) because I am using a hashmap to store the frequency of occurence of each element. 
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            ans[nums[i]]++;
+        }
+        
+        for(auto x:ans)
+        {
+            if(x.second>=2)
+            {
+                return true;
+                break;
+            }
+        }
+        return false;
     }
 };
